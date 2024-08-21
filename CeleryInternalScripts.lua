a98c02ef8dc0f4078f4469a50ff507f4d9df9a55
Jake <jake52123@proton.me>
2024-08-21 16:37:35 +0900

    Update CeleryInternalScripts.lua

--- CeleryInternalScripts.lua
+++ CeleryInternalScripts.lua
@@ -1,3 +1,7 @@
 addscript("Dex Explorer", "rbxassetid://10878052077", "Dex Explorer (For Celery)", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/jake1325/Dex/main/dex-loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jake1325/Dex/main/dex-loader.lua"))()
+end)
+
+addscript("IYAdmin", "", "Infinite Yield Admin Commands/GUI", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
