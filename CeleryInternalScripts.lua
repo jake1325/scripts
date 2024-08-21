c8155ee6da7f7f41ec2830f552b5346333c17a3f
Jake <jake52123@proton.me>
2024-08-21 16:30:50 +0900

    Update CeleryInternalScripts.lua

--- CeleryInternalScripts.lua
+++ CeleryInternalScripts.lua
@@ -1,3 +1,3 @@
-addscript("Dex Explorer", "", "Dex Explorer (For Celery)", function()
+addscript("Dex Explorer", "rbxasset://10878052077", "Dex Explorer (For Celery)", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/jake1325/Dex/main/dex-loader.lua"))()
 end)
